9f65f02b3a023ffa30f23e852336ff78e1d29155
Vlad Shlapak <94806345+Chepman123@users.noreply.github.com>
2025-09-24 08:39:50 +0200

    Create 1.js

---
+++ 24.09/1.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const fs = require('fs');
+http.createServer(function(req,res){
+  switch(req.url){
+    case "http://localhost:8080":
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.write("Strona główna");
+        res.end();
+    break;
+    case req.url==="/json":
+        res.writeHead(200,{'Content-Type':'application/json'});
+        fs.readFile('1.json','utf-8',(err,data=>{
+          res.end(JSON.stringify(data));
+        }))
+    break;
+    case req.url==="/html":
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end("<h1>Lara top</h1>");
+    break;
+    case req.url==="/htmlFile":
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+
+        fs.readFile('1.html','utf-8',(err,data)=>{
+          res.write(data);
+        })
+        res.end();
+    break;
+  }
+}).listen(8080);
